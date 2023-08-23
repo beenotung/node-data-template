7d033bbe057b6ea8afe267cf88c54c01bf936add
Beeno Tung <aabbcc1241@yahoo.com.hk>
2023-08-23 23:42:32 +0800

    feat: support loading multiple external templates

--- example/server.ts
+++ example/server.ts
@@ -9,45 +9,67 @@ function readFileString(file: string) {
   return readFile(file).then(bin => bin.toString())
 }
 
+async function readTemplateFile(filename: string) {
+  let file = join('public', filename)
+  let html = await readFileString(file)
+  return html
+}
+
+async function expandTemplateReference(html: string) {}
+
+async function expandDataTemplate(html: string) {
+  let acc = ''
+  let remain = html
+  for (; remain.length > 0; ) {
+    let match = remain.match(/ data-template="(.+)"/)
+    if (!match) {
+      console.debug('no data-template on html file')
+      break
+    }
+    let templateName = match[1]
+
+    let startIndex = remain.lastIndexOf('<', match.index)
+    if (startIndex == -1) {
+      console.debug('failed to find start of host element')
+      break
+    }
+    let tagName = remain
+      .substring(startIndex + 1, match.index)
+      .match(/[\w-]+/)?.[0]
+    if (!tagName) {
+      console.debug('failed to find tag name of host element')
+      break
+    }
+    // TODO some elements don't have closing tags
+    let suffix = `></${tagName}>`
+    let endIndex = remain.indexOf(suffix, match.index! + match[0].length)
+    if (endIndex == -1) {
+      console.debug('failed to find end of host element')
+      break
+    }
+    let hostStart = remain.substring(startIndex, endIndex + 1)
+    let hostEnd = remain.substring(endIndex + 1, endIndex + suffix.length)
+
+    let templateHTML: string
+    if (templateName.endsWith('.html')) {
+      templateHTML = await readTemplateFile(templateName)
+    } else {
+      templateHTML = 'TODO'
+    }
+
+    let host = hostStart + templateHTML + hostEnd
+    let before = remain.substring(0, startIndex)
+    let after = remain.substring(endIndex + suffix.length)
+    acc += before + host
+    remain = after
+  }
+  return acc + remain
+}
+
 app.get('/', async (req, res, next) => {
   try {
     let html = await readFileString('public/index.html')
-    for (;;) {
-      let match = html.match(/ data-template="(.+)\.html"/)
-      if (!match) {
-        console.debug('no data-template on html file')
-        break
-      }
-      console.log(match)
-      let startIndex = html.lastIndexOf('<', match.index)
-      if (startIndex == -1) {
-        console.debug('failed to find start of host element')
-        break
-      }
-      let hostHTML = html.substring(startIndex, match.index)
-      let tagName = hostHTML.match(/[\w-]+/)?.[0]
-      if (!tagName) {
-        console.debug('failed to find tag name of host element')
-        break
-      }
-      let suffix = `></${tagName}>`
-      let endIndex = html.indexOf(suffix)
-      if (endIndex == -1) {
-        console.debug('failed to find end of host element')
-        break
-      }
-      let hostStart = html.substring(startIndex, endIndex + 1)
-      let hostEnd = html.substring(endIndex + 1, endIndex + suffix.length)
-      let filename = match[1] + '.html'
-      let file = join('public', filename)
-      let templateHTML = await readFileString(file)
-      let host = hostStart + templateHTML + hostEnd
-      console.log({ before: hostStart, after: hostEnd, templateHTML })
-      let before = html.substring(0, startIndex)
-      let after = html.substring(endIndex + suffix.length)
-      html = before + host + after
-      break
-    }
+    html = await expandDataTemplate(html)
     res.contentType('text/html')
     res.end(html)
   } catch (error) {
