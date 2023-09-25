46249e75c9f179ec2327345b71a91f937eeef2b2
Beeno Tung <aabbcc1241@yahoo.com.hk>
2023-09-25 09:54:53 +0800

    explore using playwright to do ssr

--- example/server.ts
+++ example/server.ts
@@ -1,6 +1,10 @@
-import express from 'express'
+import express, { NextFunction, Request, Response } from 'express'
+import { access, accessSync, existsSync } from 'fs'
 import { print } from 'listening-on'
-import dataTemplate from 'node-data-template'
+import { join } from 'path'
+import { chromium } from 'playwright'
+// import dataTemplate from 'node-data-template'
+import dataTemplate from '../core'
 
 let app = express()
 
@@ -34,14 +38,75 @@ let articles = [
 ]
 
 app.get('/articles', (req, res) => res.json({ articles }))
-app.get(
-  '/',
-  dataTemplate.static('public', 'index.html', () => ({ articles })),
-)
+// app.get(
+//   '/',
+//   dataTemplate.static('public', 'index.html', () => ({ articles })),
+// )
+app.use(ssr('public'))
+
+/* used 550 - 580ms */
+function ssr(dir: string) {
+  let existPaths = new Set<string>()
+  let notExistPaths = new Set<string>()
+  function checkPath({ path }: Request): boolean {
+    if (path == '/') return true
+    if (!path.endsWith('.html')) return false
+    if (existPaths.has(path)) return true
+    if (notExistPaths.has(path)) return false
+    if (existsSync(join(dir, path))) {
+      existPaths.add(path)
+      return true
+    } else {
+      notExistPaths.add(path)
+      return false
+    }
+  }
+  let browserP = chromium.launch({ headless: true })
+  let contextP = browserP.then(browser =>
+    browser.newContext({
+      userAgent: 'ssr',
+    }),
+  )
+  return async function ssrRoute(
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ) {
+    try {
+      if (req.headers['user-agent'] == 'ssr' || !checkPath(req)) {
+        next()
+        return
+      }
+      let url = origin + req.url
+      console.log('[ssr]', url)
+      let start = Date.now()
+      let context = await contextP
+      let page = await context.newPage()
+      await page.goto(url, { waitUntil: 'domcontentloaded' })
+      let networkIdle = page.waitForLoadState('networkidle')
+      await page.evaluate(() => {
+        var initRender: any
+        if (typeof initRender == 'function') {
+          initRender()
+        }
+      })
+      await networkIdle
+      let html = await page.content()
+      res.header('Content-Type', 'text/html')
+      res.end(html)
+      let end = Date.now()
+      console.log('[ssr]', url, 'used', end - start, 'ms')
+      await page.close()
+    } catch (error) {
+      next(error)
+    }
+  }
+}
 
 app.use(express.static('public'))
 
 let port = 8100
+let origin = 'http://localhost:' + port
 app.listen(port, () => {
   print(port)
 })
